7dcb1cf02a0a483f9d085d12ef7f1faf8e5280f3
Mario Zupan <mzupanmz@gmail.com>
2015-07-12 22:14:06 +0200

    change compiler to v8 and write test mapping

--- src/main/java/Intro.java
+++ src/main/java/Intro.java
@@ -1,4 +1,5 @@
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import model.Bookmark;
 import model.BoxArt;
 import model.Movie;
@@ -7,15 +8,21 @@ import model.MovieList;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Intro {
 
     public static void main(String[] args) {
-        // create basic map function
-        // assert stuff
+        // project array of videos to [{id, title}]
+        List<Movie> movies = getMovies();
+        List<Map> result = movies.stream().map((movie) ->
+                ImmutableMap.of("id", movie.getId(), "title", movie.getTitle())).collect(Collectors.toList());
+        System.out.println(result);
+
     }
 
-    private List<Movie> getMovies() {
+    private static List<Movie> getMovies() {
         List<Movie> result = new ArrayList<Movie>();
         result.add(new Movie(70111470,
                 "Die Hard",
@@ -34,7 +41,7 @@ public class Intro {
                 ),
                 "http://api.netflix.com/catalog/titles/movies/70111470",
                 5.0,
-                ImmutableList.of(new Bookmark(432534, new Date()));
+                ImmutableList.of(new Bookmark(432534, new Date()))));
         result.add(new Movie(65432445,
                 "The Chamber",
                 ImmutableList.of(
@@ -53,12 +60,12 @@ public class Intro {
                 ),
                 "http://api.netflix.com/catalog/titles/movies/70111470",
                 5.0,
-                ImmutableList.of(new Bookmark(432534, new Date()));
-        );
+                ImmutableList.of(new Bookmark(432534, new Date()))
+        ));
         return result;
     }
 
-    private List<MovieList> getMovieLists() {
+    private static List<MovieList> getMovieLists() {
         List<MovieList> result = new ArrayList<MovieList>();
         result.add(new MovieList("New Releases", getMovies().subList(0, 1)));
         result.add(new MovieList("Dramas", getMovies().subList(2, 3)));
