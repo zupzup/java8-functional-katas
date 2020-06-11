f63f8ee484a0f806971bba28a674b8f279cd5a7c
Andras Torok <>
2020-06-11 14:42:18 +0200

    Wrote a test for Kata1.

--- src/test/java/katas/Kata1Test.java
+++ src/test/java/katas/Kata1Test.java
@@ -1,16 +1,44 @@
 package katas;
 
-import org.hamcrest.CoreMatchers;
-import static org.hamcrest.CoreMatchers.*;
-
-import org.junit.Assert;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
 import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 
 
 public class Kata1Test {
 
+    private static final int SIZE_OF_RETURNED_MOVIES = 4;
+
+    private static final String KEY_MOVIE_ID = "id";
+    private static final String KEY_MOVIE_TITLE = "title";
+
+    private final List<Map> moviesWithIDsForAssertion = ImmutableList.of(
+            ImmutableMap.of(KEY_MOVIE_ID, 70111470, KEY_MOVIE_TITLE, "Die Hard"),
+            ImmutableMap.of(KEY_MOVIE_ID, 654356453, KEY_MOVIE_TITLE, "Bad Boys"),
+            ImmutableMap.of(KEY_MOVIE_ID, 65432445, KEY_MOVIE_TITLE, "The Chamber"),
+            ImmutableMap.of(KEY_MOVIE_ID, 675465, KEY_MOVIE_TITLE, "Fracture")
+    );
+
     @Test
-    public void testExecute() {
-        Assert.assertThat(Kata1.execute().size(), equalTo(4));
+    public void testSizeOfTheReturnedMovieList() {
+        assertEquals(Kata1.execute().size(), SIZE_OF_RETURNED_MOVIES);
+    }
+
+    @Test
+    public void testContentOfReturnedMovies() {
+        List<Map> moviesWithIDs = Kata1.execute();
+
+        for(int index = 0; index < moviesWithIDs.size(); index++) {
+            Map movieWithID = moviesWithIDs.get(index);
+            Map movieWithIDForAssertion = moviesWithIDsForAssertion.get(index);
+
+            assertEquals(movieWithID.get(KEY_MOVIE_ID), movieWithIDForAssertion.get(KEY_MOVIE_ID));
+            assertEquals(movieWithID.get(KEY_MOVIE_TITLE), movieWithIDForAssertion.get(KEY_MOVIE_TITLE));
+        }
     }
 }
