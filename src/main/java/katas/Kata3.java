98543b4e864fb9a68d8158a7f4dc0b81f65d0882
Andras Torok <>
2020-06-11 15:58:16 +0200

    Kata3 is done.

--- src/main/java/katas/Kata3.java
+++ src/main/java/katas/Kata3.java
@@ -1,11 +1,12 @@
 package katas;
 
-import com.google.common.collect.ImmutableList;
 import model.Movie;
 import model.MovieList;
 import util.DataUtil;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
     Goal: Use map() and flatMap() to project and flatten the movieLists into an array of video ids (flatMap(c -> c.stream()))
@@ -14,8 +15,11 @@ import java.util.List;
 */
 public class Kata3 {
     public static List<Integer> execute() {
-        List<MovieList> movieLists = DataUtil.getMovieLists();
-
-        return ImmutableList.of(1, 2, 3);
+        return DataUtil.getMovieLists()
+                .stream()
+                .map(MovieList::getVideos)
+                .flatMap(Collection::stream)
+                .map(Movie::getId)
+                .collect(Collectors.toUnmodifiableList());
     }
 }
