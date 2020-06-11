5131ca330277d2297cc8fb73d64d1602068062b3
Andras Torok <>
2020-06-11 15:26:26 +0200

    Added a test to Kata2Test.
    Also removed the size constant from the Constant file.

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -1,9 +1,6 @@
 package util;
 
 public class Constants {
-
-    public static final int SIZE_OF_RETURNED_MOVIES = 4;
-
     public static final String KEY_MOVIE_ID = "id";
     public static final String KEY_MOVIE_TITLE = "title";
 }
