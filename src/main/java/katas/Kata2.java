08e75588228e9e4289ece6061d1a542c2a36bfce
Andras Torok <>
2020-06-11 15:35:20 +0200

    Kata2 is done.

--- src/main/java/katas/Kata2.java
+++ src/main/java/katas/Kata2.java
@@ -1,12 +1,10 @@
 package katas;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 import model.Movie;
 import util.DataUtil;
 
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Chain filter() and map() to collect the ids of videos that have a rating of 5.0
@@ -15,8 +13,10 @@ import java.util.Map;
 */
 public class Kata2 {
     public static List<Integer> execute() {
-        List<Movie> movies = DataUtil.getMovies();
-
-        return ImmutableList.of(1, 2, 3);
+        return DataUtil.getMovies()
+                .stream()
+                .filter(movie -> movie.getRating().equals(5.0))
+                .map(Movie::getId)
+                .collect(Collectors.toUnmodifiableList());
     }
 }
