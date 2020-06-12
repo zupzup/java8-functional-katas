e6b32d5613c66253dd1db0ff154579fd597020e1
Andras Torok <>
2020-06-12 10:05:47 +0200

    Kata6 is done.

--- src/main/java/katas/Kata6.java
+++ src/main/java/katas/Kata6.java
@@ -1,9 +1,10 @@
 package katas;
 
+import model.BoxArt;
 import model.Movie;
 import util.DataUtil;
 
-import java.util.List;
+import java.util.Collection;
 
 /*
     Goal: Retrieve the url of the largest boxart using map() and reduce()
@@ -12,8 +13,12 @@ import java.util.List;
 */
 public class Kata6 {
     public static String execute() {
-        List<Movie> movies = DataUtil.getMovies();
-
-        return "someUrl";
+        return DataUtil.getMovies()
+                .stream()
+                .map(Movie::getBoxarts)
+                .flatMap(Collection::stream)
+                .reduce(new BoxArt(0, 0, ""), (boxArt, boxArt2)
+                        -> (boxArt.getWidth() >= boxArt2.getWidth()) && (boxArt.getHeight() >= boxArt2.getHeight()) ? boxArt : boxArt2)
+                .getUrl();
     }
 }
