940d0ab60f4d044b43e5d47777eb5d864907a432
Ehtiram Etibarovich <ehtiramabdullayev@gmail.com>
2020-07-25 18:39:52 +0200

    second kata bug fix

--- src/main/java/katas/Kata2.java
+++ src/main/java/katas/Kata2.java
@@ -1,12 +1,10 @@
 package katas;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
 import model.Movie;
 import util.DataUtil;
 
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Chain filter() and map() to collect the ids of videos that have a rating of 5.0
@@ -16,7 +14,6 @@ import java.util.Map;
 public class Kata2 {
     public static List<Integer> execute() {
         List<Movie> movies = DataUtil.getMovies();
-
-        return ImmutableList.of(1, 2, 3);
+        return movies.stream().filter(movie -> movie.getRating().equals(5.0)).map(Movie::getId).collect(Collectors.toList());
     }
 }
